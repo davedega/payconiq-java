0d48f6c887fb8893ad80dba7006469796e252dab
davedega <dasher_@hotmail.com>
2018-04-11 15:28:52 +0200

    Add project, logo and colors

---
+++ app/src/main/java/com/dega/payconiq/PayconiqActivity.java
@@ -0,0 +1,13 @@
+package com.dega.payconiq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PayconiqActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payconiq);
+    }
+}
