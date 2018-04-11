9ead177d7c63a731ff8e0ff257b456975388d0cc
davedega <dasher_@hotmail.com>
2018-04-11 16:15:54 +0200

    Add HTTP API Add Retrofit and RxAndroid libraries

---
+++ app/src/main/java/com/dega/payconiq/api/ReposApi.java
@@ -0,0 +1,16 @@
+package com.dega.payconiq.api;
+
+import com.dega.payconiq.model.Repository;
+
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import rx.Observable;
+
+/**
+ * Created by davedega on 11/04/18.
+ */
+
+public interface ReposApi {
+    @GET("/users/JakeWharton/repos?page=1")
+    Observable<Repository> loadRepositories(@Header("X-Api-Authentication") String token);
+}
