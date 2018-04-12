19f35759034dc3f5caf5ee11e560cfdbea65d18e
davedega <dasher_@hotmail.com>
2018-04-12 12:07:21 +0200

    Add Realm

--- app/src/main/java/com/dega/payconiq/Application.java
+++ app/src/main/java/com/dega/payconiq/Application.java
@@ -3,6 +3,10 @@ package com.dega.payconiq;
 import com.dega.payconiq.infrastructure.DaggerPayconiqComponent;
 import com.dega.payconiq.infrastructure.PayconiqComponent;
 import com.dega.payconiq.infrastructure.PayconiqModule;
+import com.dega.payconiq.model.Repository;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
 
 /**
  * Created by davedega on 11/04/18.
@@ -19,6 +23,17 @@ public class Application extends android.app.Application {
     public void onCreate() {
         super.onCreate();
         component = DaggerPayconiqComponent.builder().payconiqModule(new PayconiqModule(this)).build();
+        Realm.init(this);
+        RealmConfiguration realmConfig = new RealmConfiguration.Builder()
+                .initialData(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm realm) {
+                        realm.createObject(Repository.class);
+                    }
+                })
+                .build();
+        Realm.deleteRealm(realmConfig); // Delete Realm between app restarts.
+        Realm.setDefaultConfiguration(realmConfig);
     }
 
 }
